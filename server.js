e9b5cd1fc9bdc7fc19f5f8c8378cf297cb9d590e
Pravin Kunhiraman <pravin.kunhiraman@gmail.com>
2020-11-27 15:32:01 +1100

    add login POST route

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ const mockUserData=[
     {name:'Jill'}
 ]
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.post('/login',function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = 'billyTheKid';
+    const mockPassword = 'superSecret';
+
+    if (username === mockUsername && password === mockPassword){
+        res.json({
+            success: true,
+            message: 'password and username match',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
+
 app.get('/users',function(req,res){
     res.json({
         success:true,
